23f3f8fdec00290d9da7fe3deddb3b191192d77b
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-22 16:17:52 -0600

    Fill products array in component from service

--- src/app/products/components/product.component.ts
+++ src/app/products/components/product.component.ts
@@ -1,11 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+
+  public products: Product[] = [];
+
   private productService = inject(ProductService);
+
+  ngOnInit(): void {
+      this.productService.findAll().subscribe(products =>{
+        this.products =  products; //fill products from service
+      });
+  }
+
 }
